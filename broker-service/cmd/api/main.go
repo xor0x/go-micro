54098f2b9765ffae05a373b658189859642e8c58
Alex Misyuk <Alexm@talgil.local>
2022-08-29 07:34:03 +0300

    second commit

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-const webPort = "80"
+const webPort = "8000"
 
 type Config struct{}
 
--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		
 		render(w, "test.page.html")
 	})
 
